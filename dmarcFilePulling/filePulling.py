9a20e02c524a47c1d5a345f5915c8a7195469dd8
LuminXY802 <rwr1971@rit.edu>
2026-05-11 21:03:59 -0400

    added in looping for parsing data in xml files

--- dmarcFilePulling/filePulling.py
+++ dmarcFilePulling/filePulling.py
@@ -7,7 +7,7 @@ directory_path = "/mnt/hdd/scriptingPractice/dmarcFilePulling/reports"
 
 # Global Variables for work and worksheet for spreadsheet file
 workbook = Workbook()
-workbook.save(filename="parsedData.xlsx")
+worksheet = workbook.active
 
 for filename in os.listdir(directory_path):
     if filename.endswith('.xml'):
@@ -15,10 +15,40 @@ for filename in os.listdir(directory_path):
         try:
             tree = ET.parse(file_path)
             root = tree.getroot()
-             # Process root element here
+            for metadata in root:
+                metadata = root.find("report_metadata")
+                for report_metadata in metadata:
+                    org_name = metadata.find("org_name").text
+                    email = metadata.find("email").text
+                    report_id = metadata.find("report_id").text
+                worksheet.append([org_name, email, report_id])
+            for public in root:
+                public = root.find("public_published")
+                if public is not None:
+                    for public_published in public:
+                        domain = public_published.find("domain").text
+                        adkim = public_published.find("adkim").text
+                        aspf = public_published.find("aspf").text
+                        p = public_published.find("p").text
+                        sp = public_published.find("sp").text
+                        pct = public_published.find("pct").text
+                    worksheet.append([domain, adkim, aspf, p, sp, pct])
+            for record in root:
+                record = root.find("row")
+                if record is not None:
+                    for row in record:
+                        source_ip = row.find("source_ip").text
+                        count = row.find("count").text
+                        disposition = row.find("disposition").text
+                        dkim = row.find("dkim").text
+                        spf = row.find("spf").text
+                        header_from = row.find("header_from").text
+                    worksheet.append([source_ip, count, disposition, dkim, spf, header_from])
             print(f"Processed: {file_path}")
         except ET.ParseError as e:
             print(f"Error parsing {file_path}: {e}")
 
 
+
+workbook.save(filename="parsedData.xlsx")
 
